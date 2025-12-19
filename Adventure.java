b0d7316557621f4754b68b9c86913f6aa06bca3c
voorheesvikings <ramji20273736@student.nhvweb.net>
2025-12-19 22:23:22 +0000

    feat:adventure start

---
+++ Adventure.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Adventure {
+
+
+    public static void main(String[] args) {
+
+        boolean gameLoop = true;
+        int score = 0;
+        String heroName;
+
+        while (gameLoop) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter your name Jomviking: ");
+            heroName = input.nextLine();
+
+            System.out.println("Hello" + heroName + "I have a query for you" + );
+            String decision1 = input.nextLine();
+
+            if (decision1.equals(Yes")) {
+                System.out.println("You Lost! Try Again");
+                return;
+            } else if (decision1.equals("No")) {
+                System.out.println();
+                String decision2 = input.nextLine();
+
+                if (decision2.eqals("Yes")) {
+                    System.out.println("You lost")
+                } else if (decision2.equals("No")) {
+                    System.out.println();
+                    String decision3 = input.nextLine();
+
+                    if (decision3.equals("))
+                }
+            }
+            
+
+
+
+
+        }
+
+
+    }
+}
